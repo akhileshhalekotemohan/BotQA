78f43d592f1f533fe263a34d79155f3a0bceaf1b
Akhilesh HM <akhilu15@gmail.com>
2021-09-04 22:02:51 +0200

    Added firefox and ie driver

---
+++ src/test/java/pageobjects/HomePageObject.java
@@ -0,0 +1,10 @@
+package pageobjects;
+
+/**
+ * All Pages should have respective page objects and
+ * these page objects should have elements which represents each element
+ * on web page
+ * Also there should be method which performs an action on an element
+ */
+public class HomePageObject {
+}
