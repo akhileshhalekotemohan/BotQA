72dcabe1c3a6d95034ca643bedbe5e3aca8fa196
Akhilesh HM <akhilu15@gmail.com>
2021-09-16 21:30:33 +0200

    Added excel writing capability

--- src/test/java/tests/TestInit.java
+++ src/test/java/tests/TestInit.java
@@ -7,9 +7,10 @@ import org.testng.Assert;
 
 public class TestInit {
     private DriverManager driverManager;
-    private WebDriver webDriver;
+    protected WebDriver webDriver;
     private final String runningOS = System.getProperty("os");
     private final String runningBrowser = System.getProperty("browser");
+    protected final String websiteUrl = "https://gallant-fermat-d0dc04.netlify.app";
 
     public void TestSetup(){
         driverManager = DriverManagerFactory.getDriverManager(runningBrowser,runningOS);
@@ -26,4 +27,6 @@ public class TestInit {
     protected void assertTitle(String expected){
         Assert.assertEquals(expected,webDriver.getTitle());
     }
+
+
 }
